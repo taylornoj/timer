774d2c5fba3d1b4ff25416dd4325bb0d461a530f
Taylor Noj <taylornoj@gmail.com>
2021-11-02 19:37:15 +0000

    pseudo code for timer

---
+++ timer1.js
@@ -0,0 +1,10 @@
+process.stdout.write('\x07');
+
+// have to slice(2) to take off first two lines of argv
+// edge cases we want to filter out:
+//  no numbers are provided - shouldn't beep at all
+//  negative numbers - ignore/skip
+//  NaN - ignore/skip
+// we want to sort the time/command line args
+// convert command line args to 1000's
+// setTimeout with  process.stdout.write('\x07');
